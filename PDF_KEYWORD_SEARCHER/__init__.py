af2ef65edce8ae42e1eb40eee7cdff0dafb94baf
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-07-05 15:28:52 +0800

    Update __init__.py

--- PDF_KEYWORD_SEARCHER/__init__.py
+++ PDF_KEYWORD_SEARCHER/__init__.py
@@ -3,3 +3,5 @@ from .core import search_in_pdfs, analyze_results
 __version__ = '0.1'
 
 __all__ = ['search_in_pdfs', 'analyze_results']
+
+print("Package 'PDF_KEYWORD_SEARCHER' has been imported.")
