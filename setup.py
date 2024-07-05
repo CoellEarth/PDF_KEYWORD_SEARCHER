c37472edf359410dbf004109919001c6720f78bb
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-07-05 15:27:54 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PDF_KEYWORD_SEARCHER',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'PyMuPDF',
+    ],
+)
