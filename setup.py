a3b3cb0182eb141f2e18b3a3828b377ddd07c967
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-07-05 16:18:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDF_KEYWORD_SEARCHER',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'PyMuPDF',
     ],
+    entry_points={
+        'console_scripts': [
+            'pdf-keyword-search = PDF_KEYWORD_SEARCHER.core:main',
+        ],
+    },
 )
+
